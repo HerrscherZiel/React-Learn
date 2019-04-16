2c5195285f515f511192e77cd5adb700f8945f4f
HerrscherZiel@gmail <HerrscherZiel@gmail.com>
2019-04-16 13:34:30 +0700

    add Api

--- src/App.js
+++ src/App.js
@@ -1,30 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Product from "./Pros";
 import Kons from './kons.js';
 import Buttons from './ButtonFloats';
 import './materialize/css/materialize.min.css';
 
-// import '.materialize/css/materialize.css';
-// import '.materialize/js/materialize.js';
-
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+
+      product: []
+    };
+  }
+
+  componentDidMount() {
+    let api_url = 'https://my-json-server.typicode.com/HerrscherZiel/product_react_api/product';
+    fetch(api_url)
+          .then(response => response.json())
+          .then(data=> {
+            this.setState({product : data});
+          })
+          .catch(function(e) {
+            console.log(e);
+          })
+      }
+
+      hapusBaris = (key) => {
+        this.state.product.splice(key, 1);
+        this.setState( {product : this.state.product});
+      }
+
+      handleSubmit = (e) => {
+        e.preventDefault();
+
+        let nama=this.ref.nama.value;
+        let harga=this.ref.harga.value;
+        let id=this.ref.id.value;
+
+        this.state.product.push({id,nama,harga});
+        this.setState({product: this.state.product});
+
+        this.refs.form.reset();
+
+      }
+
   render() {
     return (
       <div className="App">
-          {/* <div className="ndas-ndas">
-
-            <div className="iklan">
-            <h1>Promo</h1>
-            <img src={logo} className="App-logo" alt="logo"></img></div>
-
-            <div className="bayar">
-            <h1>Pembayaran</h1>
-            <img src={logo} className="App-logo" alt="logo"></img></div>
-
-          </div> */}
-
+          
           <header className="row">
           
             <Buttons isi="This is it"></Buttons>
@@ -32,21 +57,51 @@ class App extends Component {
           
           </header>
 
-          <header className="row">
+          {/* <header className="row">
           <div className="sucks">
             <Kons judul="My Life" subs="Sucks" you="you all"/>
             <Kons judul="My Life" subs="Sucks" you="yes you"/>
             <Kons judul="My Life" subs="Sucks" you="especially you"/>       
           </div>
-          </header>
+          </header> */}
+
+        <div className="col m12">
+
+          <form ref="form" onSubmit={this.handleSubmit}>
+            <input type="text" id="id" placeholder="ID Product"></input>
+            <input type="text" id="nama" placeholder="Nama Product"></input>
+            <input type="text" id="harga" placeholder="Harga"></input>
+            <button type="submit" className="waves-effect waves-light btn">Submit</button>
+          </form>
+
+        </div>
 
          <header className="row">
-          
-          <Product judul="Power Bank" harga="100.000"/>
-          <Product judul="Power Bank" harga="100.000"/>
-          <Product judul="Power Bank" harga="100.000"/>
-          <Product judul="Power Bank" harga="100.000"/>
-          
+
+            <table>
+
+              <thead>
+                <tr>
+                  <th>Nama Produk</th>
+                  <th>Harga</th>
+                  <th>Opsi</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {this.state.product.map((data,key)=>{
+                  return(
+                    <tr key={data.id}>
+                      <td>{data.nama}</td>
+                      <td>{data.harga}</td>
+                      <td><button onClick={ ()=> this.hapusBaris(key) }className="waves-effect waves-light btn">Delete</button></td>
+                    </tr>                    
+                    )
+                  })}
+              </tbody>
+
+            </table>
+
         </header> 
         
       </div>
--- src/Pros.js
+++ src/Pros.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './materialize/css/materialize.min.css';
+import bawang from './images/bawang.gif';
 
 
 class Product extends Component{
@@ -12,10 +13,10 @@ class Product extends Component{
             <div className="card-panel blue lighten-2">
 
                 <div className="center-align">
-                <img src={logo} className="App-logo" alt="logo"></img>
+                <img src={bawang} className="App-logo" alt="logo"></img>
                 </div>
 
-                <p class="flow-text"><blockquote>{this.props.judul}</blockquote>Harga : {this.props.harga} </p>                
+                <p class="flow-text"><blockquote>{this.props.nama}</blockquote>Harga : {this.props.harga} </p>                
             </div> 
         </div>
     );
